96e5b340b5c66c48b842b9701009ce4caf36b422
ilhamhakim <m.ilhamhakim@gmail.com>
2022-07-14 09:37:40 +0700

    pdf && code editor

---
+++ resources/views/ide.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Java Online IDE</title>
+    <script src="http://ajaxorg.github.io/ace-builds/src/ace.js">
+    </script> 
+    <script src="http://ajaxorg.github.io/ace-builds/src/ext-language_tools.js">
+    </script> 
+    <style type="text/css" media="screen">
+        #editor { 
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+        }
+    </style>
+
+</head>
+<body>
+    <div id="editor">    public class NetworkingCourse {
+        public static void main(String[] args) {
+            //Kerjakan Disini
+        }
+    }
+    </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.5/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/java");
+</script>
+</body>
+</html>
+
---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PDF</title>
+</head>
+<body>
+    <object data="1.pdf" type="application/pdf" width="100%" height="100%">
+        <p>Alternative text - include a link <a href="1.pdf">to the PDF!</a></p>
+    </object>
+</body>
+</html>
